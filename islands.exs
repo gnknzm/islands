a26ff0f7bfff55e735bc7eafae6a7cbe8974676e
sjcl <jrhigasinihonn.0601@gmail.com>
2019-06-17 19:02:07 +0900

    Fix indent

--- islands.exs
+++ islands.exs
@@ -5,19 +5,19 @@ defmodule Islands do
 
   def loop(map) do
     _ = IO.gets "Enterで島を抽選します"
-	map |> reserve_island |> loop
+    map |> reserve_island |> loop
   end
 
   def reserve_island(map) do
-	{island, sheet} = Enum.random(map)
+    {island, sheet} = Enum.random(map)
     remaining = sheet - 1
-	IO.puts "#{island}に座りましょう 残り#{remaining}席"
+    IO.puts "#{island}に座りましょう 残り#{remaining}席"
 
     case remaining do
-	  0 ->
-	    Map.delete(map, island)
-	  _ ->
-	    %{map | island => remaining}
+      0 ->
+        Map.delete(map, island)
+      _ ->
+        %{map | island => remaining}
     end
   end
 end
