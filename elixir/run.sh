5bb17ff06e0982c790ea35e3440bc98d8d333434
sjcl <jrhigasinihonn.0601@gmail.com>
2019-06-21 10:42:52 +0900

    Rename elixir container

--- elixir/run.sh
+++ elixir/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
- docker run -it --rm --name islands -v "$PWD":/usr/src/islands -w /usr/src/islands elixir elixir islands.exs
+ docker run -it --rm --name elixir-islands -v "$PWD":/usr/src/islands -w /usr/src/islands elixir elixir islands.exs
